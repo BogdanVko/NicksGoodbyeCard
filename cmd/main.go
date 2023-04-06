349e3d9d1b2c16c51b1a579b7abef27e1302c257
Bogdan Vasilchenko <beeer01net@gmail.com>
2023-04-06 13:39:03 -0400

    small refactor to make things easier

--- cmd/main.go
+++ cmd/main.go
@@ -13,43 +13,6 @@ type GoodbyeNickMessagePacket struct {
 	goodbyeMessages []string
 }
 
-var (
-	BogdansGoodbyeWordsToNick = GoodbyeNickMessagePacket{
-		colleagueName: "Bogdan",
-		goodbyeMessages: []string{
-			"Good luck in your next adventure!",
-			"Your hard work will always be remembered.",
-			`It was a great pleasure to work with you Nick, I highly appriciate you helping me all the time when I just started. 
-	One could not have asked for a better colleage and mentor.`, // this is a long-style message
-			"something else here",
-		},
-	}
-
-	JennasGoodbyeWordsToNick = GoodbyeNickMessagePacket{
-		colleagueName: "Jenna",
-		goodbyeMessages: []string{
-			"Good luck in your next adventure!",
-			//Add/rm  stuff :)
-		},
-	}
-
-	JoesGoodbyeWordsToNick = GoodbyeNickMessagePacket{
-		colleagueName: "Joe",
-		goodbyeMessages: []string{
-			"Good luck in your next adventure!",
-			//Add/rm stuff :)
-		},
-	}
-
-	StephensGoodbyeWordsToNick = GoodbyeNickMessagePacket{
-		colleagueName: "Stephen",
-		goodbyeMessages: []string{
-			"Good luck in your next adventure!",
-			//Add/rm  stuff :)
-		},
-	}
-)
-
 func main() {
 	nick()
 }
