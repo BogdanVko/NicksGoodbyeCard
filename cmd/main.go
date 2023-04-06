e3cbe88b889a9992874c094333fac68020af6206
Bogdan Vasilchenko <beeer01net@gmail.com>
2023-04-06 13:46:16 -0400

    fixed

--- cmd/main.go
+++ cmd/main.go
@@ -8,10 +8,49 @@ import (
 	"github.com/fatih/color"
 )
 
-func main() {
-	nick()
+type GoodbyeNickMessagePacket struct {
+	colleagueName   string   // first or first&last go here, each person will be picked at random
+	goodbyeMessages []string // each message will be picked at randmon
 }
-func nick() {
+
+var (
+	BogdansGoodbyeWordsToNick = GoodbyeNickMessagePacket{
+		colleagueName: "Bogdan",
+		goodbyeMessages: []string{
+			"Good luck in your next adventure!",
+			"Your hard work will always be remembered.",
+			`It was a great pleasure to work with you Nick, I highly appriciate you helping me all the time when I just started. 
+	One could not have asked for a better colleage and mentor.`, // this is a long-style message
+			"something else here",
+		},
+	}
+
+	JennasGoodbyeWordsToNick = GoodbyeNickMessagePacket{
+		colleagueName: "Jenna",
+		goodbyeMessages: []string{
+			"Good luck in your next adventure!",
+			//Add/rm  stuff :)
+		},
+	}
+
+	JoesGoodbyeWordsToNick = GoodbyeNickMessagePacket{
+		colleagueName: "Joe",
+		goodbyeMessages: []string{
+			"Good luck in your next adventure!",
+			//Add/rm stuff :)
+		},
+	}
+
+	StephensGoodbyeWordsToNick = GoodbyeNickMessagePacket{
+		colleagueName: "Stephen",
+		goodbyeMessages: []string{
+			"Good luck in your next adventure!",
+			//Add/rm  stuff :)
+		},
+	}
+)
+
+func main() {
 	// Default text
 	color.Cyan("\n\nHey Nick!")
 	color.Cyan("As a token of our appreciation, respect and friendship, we've made you a little something!\nThis is your \"Goodbye Giftcard\" :)")
@@ -27,7 +66,7 @@ func nick() {
 
 	time.Sleep(6 * time.Second)
 
-	//Send goodbyes 🥲 ; random goodbye each time, randmon colleague as well
+	// Send goodbyes 🥲 ; random goodbye each time, randmon colleague as well
 	sendRandomGoodbyeMessages()
 }
 
